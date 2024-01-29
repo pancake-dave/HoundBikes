b819e9fc6290a5ed52def9d1e04b94071deaa7a4
Dawid <dawid.bialokryty@gmail.com>
2024-01-29 22:56:32 +0100

    rewrote js, added some animations and media queries

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
 const burgerBtn = document.querySelector('.burger-btn')
 const navBar = document.querySelector('.nav')
+const navBarItems = document.querySelectorAll('.nav__item')
 
-function handleBtn () {
-    burgerBtn.classList.toggle('burger-btn-animation')
-    navBar.classList.toggle('nav-active')
+const handleNav = () => {
+    if (burgerBtn.classList.contains('burger-btn-animation')) {
+        burgerBtn.classList.remove('burger-btn-animation')
+        navBar.classList.remove('nav-active')
+    } else {
+        burgerBtn.classList.add('burger-btn-animation')
+        navBar.classList.add('nav-active')
+    }
+
+    navBarItems.forEach(item => {
+        item.addEventListener ('click', () =>{
+            navBar.classList.remove('nav-active')
+            burgerBtn.classList.remove('burger-btn-animation')
+        })
+    })
+
 }
 
-burgerBtn.addEventListener('click', handleBtn)
+burgerBtn.addEventListener('click', handleNav)
